a8443b004b12d8c1ef817f008d2ba0302e77ab8c
Caiorossi00 <caio_rossi56@hotmail.com>
2025-05-13 10:40:58 -0300

    feat: add Home component styling and structure with responsive design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import BookList from '../components/Vitrine/BookList';
+import "../styles/Home.scss"
 
 const Home = () => {
   const [books, setBooks] = useState([]);
@@ -11,8 +12,11 @@ const Home = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Vitrine de Livros</h1>
+    <div className='container-home'>
+      <img
+        src="https://images.unsplash.com/photo-1744029829181-ad19c2ee248b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Livros"
+      />
       <BookList books={books} />
     </div>
   );
