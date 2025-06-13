67c5914cbe6b76c88b90708b4578e01584d5d783
Caiorossi00 <caio_rossi56@hotmail.com>
2025-06-13 17:34:55 -0300

    fix: improve addToCart function to prevent duplicate items and enhance user feedback

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -14,15 +14,22 @@ export const CartProvider = ({ children }) => {
   }, [cart]);
 
   const addToCart = (book) => {
+    if (cart.some((item) => item.id === book.id)) {
+      alert("Este livro já está no carrinho!");
+      return;
+    }
+
     const precoNumero = Number(book.price.replace(",", "."));
     setCart((prev) => {
       const newCart = [...prev, { ...book, price: precoNumero }];
       console.log(
-        "Itens no carrinho:",
+        "Itens no carrinho após adição:",
         newCart.map((item) => item.title)
       );
       return newCart;
     });
+
+    alert("Adicionado ao carrinho!");
   };
 
   const removeFromCart = (id) => {
